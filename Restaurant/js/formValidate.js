596b8a75da57c83fb2fd2c0fdbc34a70a853f6b2
sugdiwa <suganya.diwakar@gmail.com>
2016-01-21 12:20:00 +1100

    added javascript  and styles

--- Restaurant/js/formValidate.js
+++ Restaurant/js/formValidate.js
@@ -1,14 +1,12 @@
-//Validating the contact us and Feedback Form on submission
+//Validating the  Feedback Form on submission
 $(document).ready(function()
 {
 	/*call the submitFeedback funtion when we click the submit
     button in feedback*/
 	$('#submit-btn').on('click',submitFeedback);
-	/*call the submitContact funtion when we click the submit
-    button in Contact us*/
-	$('#submit-btn-contact').on('click',submitContact);
-
+	
 	function submitFeedback(){
+
 		//getting the feed values
 		var feedName = $('#feedName').val();
 		var feedEmail = $('#feedEmail').val();
@@ -18,7 +16,7 @@ $(document).ready(function()
 		//Checking whether all the fields are entered
 		if(feedName === "" || feedName === " ") {
 		 	dispAlert('alert',"Please Enter the name");
-		 	feedName.focus(); 
+		 	$('#feedName').focus(); 
 		 	return false;
 		}
 		else if(feedEmail === "" || feedEmail === " ") {
@@ -26,9 +24,13 @@ $(document).ready(function()
 			feedEmail.focus();
 		 	return false; 
 		}
-		
+		else if(feedNum === "" || feedNum === " ") { 
+			dispAlert('alert',"Please Enter the Contact No"); 
+			feedNum.focus();
+			return false;  
+		}
 		else if(feedLike === "" || feedLike === " ") { 
-			dispAlert('alert',"Please Enter What food item did you enjoy most during your visit"); 
+			dispAlert('alert',"Please Enter What food item did you like"); 
 			feedLike.focus();
 			return false;
 		}
@@ -37,77 +39,14 @@ $(document).ready(function()
 			feedMsg.focus();
 			return false;  
 		}
-		else if(feedNum === "" || feedNum === " ") { 
-			dispAlert('alert',"Please Enter the Contact No"); 
-			feedNum.focus();
-			return false;  
-		}
+		
 		else{
 			removeAlert('alert');
 			alert("submitted successfully");
 		}
 		return false;
 	}
-	function submitContact(){
-		//getting the contact us form values
-		var conName = $('#conName').val();
-		var conEmail = $('#conEmail').val();
-		var conNum = $('#conNum').val();
-		var conMsg = $('#conMsg').val();
-		var conComment = $('#conComments').val();
-		//Checking whether all the fields are entered
-		if(conName === "" || conName === " ") {
-		 	dispAlert('alertContact',"Please Enter the name"); 
-		 	return false;
-		}
-		else if(conEmail === "" || conEmail === " ") {
-			
-			dispAlert('alertContact',"Please Enter the mail"); 
-		 	return false; 
-		}
-		else if(conNum === "" || conNum === " ") { 
-			dispAlert('alertContact',"Please Enter the Contact No"); 
-			return false;  
-		}
-		else if(conMsg === "" || conMsg === " ") { 
-			dispAlert('alertContact',"Please Enter the Message "); 
-			return false;
-		}
-		else if(conComment === "" || conComment === " "){
-			dispAlert('alertContact',"Please Enter the comments/suggestions"); 
-			return false;  
-		}
-		else{
-			removeAlert('alertContact');
-		}
-		 if(conEmail !="" || conEmail != " "){
-			if (conEmail.search( /^[a-zA-Z]+([_\.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([\.-]?[a-zA-Z0-9]+)*(\.[a-zA-Z]{2,4})+$/ ) == -1)
-			{
-				dispAlert('alertContact',"Please Enter the mail correctly"); 
-			}
-			else{
-				removeAlert('alertContact');
-			}
-		}
-		if(conNum != "" && conNum != " ")
-		{
-		
-		if(conNum.search(/^[0-9]+$/) >= 0)
-		{
-			if (conNum.length < 7)
-			{
-				dispAlert('alertContact',"Please Enter the Number correctly");
-				
-			}
-		}
-		else
-		{
-			removeAlert('alertContact');
-		}	
-	}
-		
-		return false;
-	}
+	
 	//function for displaying alerts
 	function dispAlert(id,msg){
 		$('#'+id).html(msg);
--- Restaurant/js/index.js
+++ Restaurant/js/index.js
@@ -2,18 +2,18 @@
 var myCenter = new google.maps.LatLng(-33.823835,151.193164);
 /* Initialize Google Map*/
 function initialize() {
-var mapProp = {
-center:myCenter,
-zoom:17,
-scrollwheel:true,
-draggable:true,
-mapTypeId:google.maps.MapTypeId.ROADMAP
+  var mapProp = {
+  center:myCenter,
+  zoom:17,
+  scrollwheel:true,
+  draggable:true,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
 };
 
 var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
 var marker = new google.maps.Marker({
-position:myCenter,
+  position:myCenter,
 });
 
 marker.setMap(map);
@@ -24,40 +24,45 @@ window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/
  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
 (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 s.parentNode.insertBefore(g,s)}(document,'script'));
-
+/*-------------------------------------------------------------------------------------------------------*/
+//Array for contact page images
 var arr = [];
-  arr[0]= new Image();
-  arr[0].src = "img/contactimg1.jpg";
+arr[0]= new Image();
+arr[0].src = "img/contactimg1.jpg";
 
-  arr[1]= new Image();
-  arr[1].src = "img/contactimg2.jpg";
+arr[1]= new Image();
+arr[1].src = "img/contactimg2.jpg";
 
-  arr[2]= new Image();
-  arr[2].src = "img/contactimg3.jpg";
-  
-  arr[3]= new Image();
-  arr[3].src = "img/contactimg6.jpg";
- 
-  arr[4]= new Image();
-  arr[4].src = "img/contactimg7.jpg";
-  
-  arr[5]= new Image();
-  arr[5].src = "img/contactimg8.jpg";
-  
-  arr[6]= new Image();
-  arr[6].src = "img/contactimg9.jpg";
-  
-  arr[7]= new Image();
-  arr[7].src = "img/contactimg10.jpeg";
-  var i=0;
-  function slide(){
-    document.getElementById("image1").src= arr[i].src;
-    i++;
-    if(i==arr.length){
-        i=0;
-    }
-    setTimeout(function(){ slide(); },8000);
+arr[2]= new Image();
+arr[2].src = "img/contactimg3.jpg";
+
+arr[3]= new Image();
+arr[3].src = "img/contactimg6.jpg";
+
+arr[4]= new Image();
+arr[4].src = "img/contactimg7.jpg";
+
+arr[5]= new Image();
+arr[5].src = "img/contactimg8.jpg";
+
+arr[6]= new Image();
+arr[6].src = "img/contactimg9.jpg";
+
+arr[7]= new Image();
+arr[7].src = "img/contactimg10.jpeg";
+var i=0;
+//onLoad function for slide show images
+function slide(){
+  document.getElementById("image1").src= arr[i].src;
+  i++;
+  if(i==arr.length){
+      i=0;
   }
+  setTimeout(function(){ slide(); },8000);
+}
+/*-------------------------------------------------------------------------------------------------------*/
+
+/*------------Jquery----------------------------------------------------------------*/
 
 $(document).ready(function(){
 
@@ -65,63 +70,61 @@ $(document).ready(function(){
 
   function changeMenu(){
    
-    //Get the City name value
+    //Get the menu name value
     var menuName=$('#menutype').val();
     
-    
-
-    /* Conditions to check which city image to display depending on 
-    the input text value  */
+    /* Conditions to check which menu image to display depending on 
+    the drop down value  */
     if(menuName === 'appetizer') {
-        $('#entree').hide();
-        $('#rice').hide();
-        $('#noodles').hide();
-        $('#deserts').hide();
-        $('#juices').hide();
-        $('#appetizer').show();
-      }
-      else if(menuName === 'entree'){
-        $('#appetizer').hide();
-        $('#entree').show();
-         $('#deserts').hide();
-         $('#juices').hide();
-        $('#noodles').hide();
-      }
-      else if(menuName === 'rice'){
-        $('#appetizer').hide();
-        $('#entree').hide();
-        $('#deserts').hide();
-        $('#noodles').hide();
-        $('#juices').hide();
-        $('#rice').show();
-        
-      }
-       else if(menuName === 'noodles'){
-        $('#appetizer').hide();
-        $('#entree').hide();
-        $('#rice').hide();
-        $('#deserts').hide();
-        $('#juices').hide();
-        $('#noodles').show();
-      }
-      else if(menuName === 'deserts'){
-        $('#appetizer').hide();
-        $('#entree').hide();
-        $('#rice').hide();
-        $('#noodles').hide();
-        $('#juices').hide();
-        $('#deserts').show();
-      }
-       else if(menuName === 'juices'){
-        $('#appetizer').hide();
-        $('#entree').hide();
-        $('#rice').hide();
-        $('#noodles').hide();
-        $('#deserts').hide();
-        $('#juices').show();
-      }
-    
+      $('#entree').hide();
+      $('#rice').hide();
+      $('#noodles').hide();
+      $('#deserts').hide();
+      $('#juices').hide();
+      $('#appetizer').slideToggle("slow");
+    }
+    else if(menuName === 'entree'){
+      $('#appetizer').hide();
+      $('#entree').slideToggle("slow");
+      $('#deserts').hide();
+      $('#juices').hide();
+      $('#noodles').hide();
+      $('#rice').hide();
+    }
+    else if(menuName === 'rice'){
+      $('#appetizer').hide();
+      $('#entree').hide();
+      $('#deserts').hide();
+      $('#noodles').hide();
+      $('#juices').hide();
+      $('#rice').slideToggle("slow");
+    }
+     else if(menuName === 'noodles'){
+      $('#appetizer').hide();
+      $('#entree').hide();
+      $('#rice').hide();
+      $('#deserts').hide();
+      $('#juices').hide();
+      $('#noodles').slideToggle("slow");
+    }
+    else if(menuName === 'deserts'){
+      $('#appetizer').hide();
+      $('#entree').hide();
+      $('#rice').hide();
+      $('#noodles').hide();
+      $('#juices').hide();
+      $('#deserts').slideToggle("slow");
+    }
+    else if(menuName === 'juices'){
+      $('#appetizer').hide();
+      $('#entree').hide();
+      $('#rice').hide();
+      $('#noodles').hide();
+      $('#deserts').hide();
+      $('#juices').slideToggle("slow");
+    }
   }
+  
   // Colorbox Call for gallery
   $("[rel^='lightbox']").prettyPhoto();
   // Initialize Tooltip
@@ -130,37 +133,38 @@ $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
   $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
 
-    // Prevent default anchor click behavior
-    event.preventDefault();
+  // Prevent default anchor click behavior
+  event.preventDefault();
 
-    // Store hash
-    var hash = this.hash;
+  // Store hash
+  var hash = this.hash;
 
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 900, function(){
-   
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (900) specifies the number of milliseconds
+  // it takes to scroll to the specified area
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 900, function(){
+ 
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
+  //for modal in catering services
+  $('li img').on('click',function(){
+    var src = $(this).attr('src');
+    var img = '<img src="' + src + '" class="img-responsive"/>';
+    $('#myModal').modal();
+    $('#myModal').on('shown.bs.modal', function(){
+      $('#myModal .modal-body').html(img);
     });
-    //for modal in catering services
-    $('li img').on('click',function(){
-        var src = $(this).attr('src');
-        var img = '<img src="' + src + '" class="img-responsive"/>';
-        $('#myModal').modal();
-        $('#myModal').on('shown.bs.modal', function(){
-            $('#myModal .modal-body').html(img);
-        });
-        $('#myModal').on('hidden.bs.modal', function(){
-            $('#myModal .modal-body').html('');
-        });
-   });
-
-    $('#nav-toggle').click(function(){
+    $('#myModal').on('hidden.bs.modal', function(){
+      $('#myModal .modal-body').html('');
+    });
+  });
+  
+  $('#nav-toggle').click(function(){
     $('body').toggleClass('nav-open');
   });
-  });
+});
 
 })
